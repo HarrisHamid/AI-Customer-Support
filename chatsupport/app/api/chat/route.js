a79a49c646d1e5c33344a099075ce92d978f63cb
Harris Hamid <harrishamid26@gmail.com>
2024-08-11 03:39:13 -0400

    Initial basic chatbot features implemented

---
+++ chatsupport/app/api/chat/route.js
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt =
+  "You are a AI-powered customer support assistant for a company";
+
+export async function POST(req) {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const data = await req.json();
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt,
+      },
+      ...data,
+    ],
+
+    model: "gpt-3.5-turbo",
+        stream: true,
+  });
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      try {
+        for await (const chunk of completion) {
+          const content = chunk.choices[0]?.delta?.content;
+
+          if (content) {
+            const text = encoder.encode(content);
+            controller.enqueue(text);
+          }
+        }
+      } catch (error) {
+        controller.error(error);
+      } finally {
+        controller.close();
+      }
+      s;
+    },
+  });
+
+  return new NextResponse(stream)
+}
